8358563a12c45dba69afe24dbc5debe3e9ff0bb8
gmp26 <gmp26@cam.ac.uk>
2017-03-10 11:49:30 +0000

    more renaming

--- server.R
+++ server.R
@@ -1,53 +1,62 @@
 library(shiny)
 library(fanplot)
 
-percentiles <- c(0.025, 0.15, 0.30, 0.70, 0.85, 0.975)
-bands <- length(percentiles)
-
-smoothing <- 10
-smooth <- function(k) { smoothing*(k - 1) + 1 }
 
 
+expand <- function(df, percentiles) {
+  k <- nrow(df)
+  bands <- length(percentiles)
+  
+  val <- matrix(NA, nrow = bands, ncol = k)
+  med <- rep(NA, k)
+  for (i in 1:k) {
+    val[, i] <- qsplitnorm(p = percentiles, 
+                           mode = df$mode[i],
+                           sd = df$sd[i],
+                           skew = df$skew[i])
+    med[i] <- qsplitnorm(p = 0.5,
+                         mode = df$mode[i],
+                         sd = df$sd[i],
+                         skew = df$skew[i])
+  }
+  
+}
+
+smooth_and_expand <- function(df, smoothing, percentiles) {
+  
+  k <- nrow(df)
+  smoothed_k <- smoothing*(k - 1) + 1
+    
+  bands <- length(percentiles)
+  
+  val <- matrix(NA, nrow = bands, ncol = k)
+  med <- rep(NA, k)
+  for (i in 1:k) {
+    val[, i] <- qsplitnorm(p = percentiles, 
+                           mode = df$mode[i],
+                           sd = df$sd[i],
+                           skew = df$skew[i])
+    med[i] <- qsplitnorm(p = 0.5,
+                         mode = df$mode[i],
+                         sd = df$sd[i],
+                         skew = df$skew[i])
+  }
+  
+  smoothed_val <- matrix(NA, nrow = bands, ncol = smoothed_k)
+  xyval <- matrix(NA, nrow = bands, ncol = k)
+  med3 <- spline(xy.coords(med), n = smoothed_k)$y
+  
+  for (j in 1:bands) {
+    xyval[j,] <- xy.coords(val[j,])$y
+    smoothed_val[j,] <- spline(xy.coords(val[j,]), n = smoothed_k)$y
+  }
+  
+  return(list(original_val = xyval, smoothed_val = smoothed_val, med = med3))
+}
 
 shinyServer(function(input, output) {
   
-  netstats <- reactive({
-  
-    inFile <- input$file1
-    if (is.null(inFile))
-      return(NULL)
-    
-    df1 <- read.csv(inFile$datapath, header = input$header)
-    
-    k <- nrow(df1)
-    
-    val <- matrix(NA, nrow = bands, ncol = k)
-    med <- rep(NA, k)
-    for (i in 1:k) {
-      val[, i] <- qsplitnorm(p = percentiles, 
-                             mode = df1$mode[i],
-                             sd = df1$sd[i],
-                             skew = df1$skew[i])
-      med[i] <- qsplitnorm(p = 0.5,
-                           mode = df1$mode[i],
-                           sd = df1$sd[i],
-                           skew = df1$skew[i])
-    }
-    
-    ik <- smooth(k)
-    smoothed_val <- matrix(NA, nrow = bands, ncol = ik)
-    xyval <- matrix(NA, nrow = bands, ncol = k)
-    med3 <- spline(xy.coords(med), n = ik)$y
-    
-    for (j in 1:bands) {
-      xyval[j,] <- xy.coords(val[j,])$y
-      smoothed_val[j,] <- spline(xy.coords(val[j,]), n = ik)$y
-    }
-    
-    return(list(val = xyval, smoothed_val = smoothed_val, med = med3))
-    
-  })
-  
+
   output$df <- renderDataTable({
     inFile <- input$file1
     if (is.null(inFile))
@@ -57,18 +66,27 @@ shinyServer(function(input, output) {
   }, options = list(filter = FALSE, searching = FALSE, paging = FALSE, info = FALSE, ordering = FALSE))
   
   output$plot <- renderPlot({
-    netstats <- netstats()
-    if (is.null(netstats)) 
+    
+    inFile <- input$file1
+    if (is.null(inFile))
       return(NULL)
-  
-    val <- netstats$val
-    smoothed_val <- netstats$smoothed_val
-    med <- netstats$med
+    
+    df1 <- read.csv(inFile$datapath, header = input$header)
+    
+    # smooth data frame values and parameters
+    # expand uncertainties
+    smoothing <- 2
+    smoothed_expanded <- smooth_and_expand(df = df1, smoothing = smoothing,
+                                  percentiles = c(0.025, 0.15, 0.30, 0.70, 0.85, 0.975))
+    
+    original_val <- smoothed_expanded$original_val
+    smoothed_val <- smoothed_expanded$smoothed_val
+    med <- smoothed_expanded$med
 
     par(mar = c(2,2,2,2))
 
     grid()
-    plot((val[3,] + val[4,])/2, type="p", pch=18, ylim = c(-10,2))
+    plot((original_val[3,] + original_val[4,])/2, type = "p", pch = 18, ylim = c(-10,2))
     
     fan(smoothed_val, data.type = "values", start = start(smoothed_val), 
         type = "interval",
